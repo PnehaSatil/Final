edee1c66b82284865195ebf418014227e570a6d7
pnehasati <pnehasatil27@gmail.com>
2021-12-29 22:25:28 +0530

    Updated Resume

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
- 
-const functionApi = 'http://localhost:7071/api/Resume';
+
+const functionApiUrl = 'https://resumecounter.azurewebsites.net/api/Resume?code=SB9lTmxQZvQRAD1JScYIonka9dzoCmL0LPLeLYAR88cwKE3/yKK5Yg==';
+const localfunctionApi = 'http://localhost:7071/api/Resume';
 
 const getVisitCount = () => {
     let count = 30 
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
